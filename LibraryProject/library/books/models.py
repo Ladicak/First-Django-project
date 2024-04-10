f8aa3c6dd8feb8d25e6dd61d7b0a6645b66b2e99
ladis <ladislav.bilik@email.cz>
2024-04-10 19:16:15 +0200

    update project

--- LibraryProject/library/books/models.py
+++ LibraryProject/library/books/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    on_stock = models.BooleanField(default=True)
+    category = models.ManyToManyField(Category)
+    author = models.ManyToManyField(Author)
+
+
+
+
